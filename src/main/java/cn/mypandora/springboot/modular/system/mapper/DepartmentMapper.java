17c0d04b4a41bbfef635bbeee1753de81c46fb5c
hankaibo <hankaibo@hotmail.com>
2019-09-29 20:58:30 +0800

    chore: 临时分支

--- src/main/java/cn/mypandora/springboot/modular/system/mapper/DepartmentMapper.java
+++ src/main/java/cn/mypandora/springboot/modular/system/mapper/DepartmentMapper.java
@@ -1,13 +1,10 @@
 package cn.mypandora.springboot.modular.system.mapper;
 
 import cn.mypandora.springboot.core.base.MyBaseMapper;
-import cn.mypandora.springboot.core.shiro.filter.FilterChainManager;
-import cn.mypandora.springboot.modular.system.model.po.Department;
 import cn.mypandora.springboot.modular.system.model.po.Department;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
-import java.util.Map;
 
 /**
  * DepartmentMapper
@@ -22,7 +19,7 @@ public interface DepartmentMapper extends MyBaseMapper<Department> {
      *
      * @return 整棵树
      */
-    List<Department> loadFullTree();
+    List<Department> listAll();
 
     /**
      * 获取某一层级节点。
@@ -31,7 +28,7 @@ public interface DepartmentMapper extends MyBaseMapper<Department> {
      * @return 指定层级的树
      * TODO SQL未实现
      */
-    List<Department> loadTreeWithLevel(int level);
+    List<Department> listByLevel(int level);
 
     /**
      * 获得本节点下面的所有后代节点
@@ -39,7 +36,7 @@ public interface DepartmentMapper extends MyBaseMapper<Department> {
      * @param id 当前操作节点id
      * @return 本节点下面的所有后代节点
      */
-    List<Department> getDescendant(Long id);
+    List<Department> listDescendants(Long id);
 
     /**
      * 获得本节点的孩子节点
@@ -47,7 +44,7 @@ public interface DepartmentMapper extends MyBaseMapper<Department> {
      * @param id 当前操作节点id
      * @return 本节点的孩子节点
      */
-    List<Department> getChild(Long id);
+    List<Department> listChildren(Long id);
 
     /**
      * 获得本节点的父节点
@@ -63,7 +60,7 @@ public interface DepartmentMapper extends MyBaseMapper<Department> {
      * @param id 当前操作节点id
      * @return 本节点的祖先节点
      */
-    List<Department> getAncestry(Long id);
+    List<Department> listAncestries(Long id);
 
     /**
      * 获得本节点的所有兄弟节点
@@ -71,44 +68,30 @@ public interface DepartmentMapper extends MyBaseMapper<Department> {
      * @param id 当前操作节点id
      * @return 本节点的所有兄弟节点
      */
-    List<Department> getSiblings(Long id);
+    List<Department> listSiblings(Long id);
 
     /**
-     * 左节点加2
+     * 父右节点加N
      *
      * @param id 节点id
      */
-    void lftPlus2(Long id);
+    void parentRgtAdd(@Param("id") Long id, @Param("amount") Integer amount);
 
     /**
-     * 右节点加2
-     *
-     * @param id 节点id
-     */
-    void rgtPlus2(Long id);
-
-    /**
-     * 父右节点加2
-     *
-     * @param id 节点id
-     */
-    void parentRgtPlus2(Long id);
-
-    /**
-     * 左节点减N
+     * 左节点加N
      *
      * @param id     节点id
      * @param amount 大于id左值的节点，左值要减去的数
      */
-    void lftMinus(@Param("id") Long id, @Param("amount") Integer amount);
+    void lftAdd(@Param("id") Long id, @Param("amount") Integer amount);
 
     /**
-     * 右节点减N
+     * 右节点加N
      *
      * @param id     节点id
      * @param amount 大于id右值的节点，右值要减去的数
      */
-    void rgtMinus(@Param("id") Long id, @Param("amount") Integer amount);
+    void rgtAdd(@Param("id") Long id, @Param("amount") Integer amount);
 
     /**
      * 当前节点集合都加上n
