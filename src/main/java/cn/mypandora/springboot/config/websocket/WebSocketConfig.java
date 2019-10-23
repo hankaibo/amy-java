e5590a07a3dfc4208e081540706d6d26382302ee
hankaibo <hankaibo@hotmail.com>
2019-10-23 19:17:18 +0800

    temp: 临时分支

---
+++ src/main/java/cn/mypandora/springboot/config/websocket/WebSocketConfig.java
@@ -0,0 +1,29 @@
+package cn.mypandora.springboot.config.websocket;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+/**
+ * WebSocketConfig
+ *
+ * @author hankaibo
+ * @date 2019/10/22
+ */
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry webSocketHandlerRegistry) {
+        webSocketHandlerRegistry.addHandler(myHandler(), "myHandler/{ID}").setAllowedOrigins("*");
+    }
+
+    @Bean
+    public WebSocketHandler myHandler() {
+        return new MyHandler();
+    }
+}
