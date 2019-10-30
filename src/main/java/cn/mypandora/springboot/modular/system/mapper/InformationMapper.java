b5984451571966deeb00eedf978c9249dde05182
hankaibo <hankaibo@hotmail.com>
2019-10-30 08:01:43 +0800

    feat: 添加发布功能

--- src/main/java/cn/mypandora/springboot/modular/system/mapper/InformationMapper.java
+++ src/main/java/cn/mypandora/springboot/modular/system/mapper/InformationMapper.java
@@ -3,6 +3,8 @@ package cn.mypandora.springboot.modular.system.mapper;
 import cn.mypandora.springboot.core.base.MyBaseMapper;
 import cn.mypandora.springboot.modular.system.model.po.Information;
 
+import java.util.List;
+
 /**
  * InformationMapper
  *
@@ -10,4 +12,11 @@ import cn.mypandora.springboot.modular.system.model.po.Information;
  * @date 2019/10/29
  */
 public interface InformationMapper extends MyBaseMapper<Information> {
+
+    /**
+     * 批量更新发布状态。
+     *
+     * @param idList id集合
+     */
+    void updateBatchPublish(long[] idList);
 }
