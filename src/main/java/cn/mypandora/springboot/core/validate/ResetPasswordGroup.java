d9da1ea5b564d2afad17c360e3d06981ccd37377
hankaibo <hankaibo@hotmail.com>
2020-05-30 16:15:38 +0800

    chore: 更新注释说明

--- src/main/java/cn/mypandora/springboot/core/validate/ResetPasswordGroup.java
+++ src/main/java/cn/mypandora/springboot/core/validate/ResetPasswordGroup.java
@@ -1,9 +1,9 @@
 package cn.mypandora.springboot.core.validate;
 
 /**
- * 分组接口：添加
+ * 分组接口：重置密码
  * 
  * @author hankaibo
  * @date 2020/5/8
  */
-public interface AddGroup {}
+public interface ResetPasswordGroup {}
