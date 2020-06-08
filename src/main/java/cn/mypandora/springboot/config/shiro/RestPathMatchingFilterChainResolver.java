a8e8429681f59111e973dbf342b8cf0298833fbf
hankaibo <hankaibo@hotmail.com>
2020-06-08 14:49:50 +0800

    fix: 修复shiro匹配路径错误问题

--- src/main/java/cn/mypandora/springboot/config/shiro/RestPathMatchingFilterChainResolver.java
+++ src/main/java/cn/mypandora/springboot/config/shiro/RestPathMatchingFilterChainResolver.java
@@ -1,12 +1,12 @@
 package cn.mypandora.springboot.config.shiro;
 
 import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 
 import org.apache.shiro.web.filter.mgt.FilterChainManager;
 import org.apache.shiro.web.filter.mgt.PathMatchingFilterChainResolver;
+import org.apache.shiro.web.util.WebUtils;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -20,13 +20,10 @@ import lombok.extern.slf4j.Slf4j;
 public class RestPathMatchingFilterChainResolver extends PathMatchingFilterChainResolver {
 
     private static final String DEFAULT_PATH_SEPARATOR = "/";
+    private static final int NUM_2 = 2;
 
     public RestPathMatchingFilterChainResolver() {
         super();
-    }
-
-    public RestPathMatchingFilterChainResolver(FilterConfig filterConfig) {
-        super(filterConfig);
     }
 
     /**
@@ -43,23 +40,28 @@ public class RestPathMatchingFilterChainResolver extends PathMatchingFilterChain
     @Override
     public FilterChain getChain(ServletRequest request, ServletResponse response, FilterChain originalChain) {
         FilterChainManager filterChainManager = this.getFilterChainManager();
+        // 没有配置shiro过滤器则直接进入servlet。
         if (!filterChainManager.hasChains()) {
             return null;
         }
-
+        // 扩写原匹配规则，在原来的路径一致的基础上加上请求方法也一致。
         String requestURI = getPathWithinApplication(request);
         requestURI = safeFormatUrl(requestURI);
 
         for (String pathPattern : filterChainManager.getChainNames()) {
             String[] pathPatternArray = pathPattern.split("==");
-            String pathPatternUrl = pathPatternArray[0];
-            pathPatternUrl = safeFormatUrl(pathPatternUrl);
-            if (pathMatches(pathPatternUrl, requestURI)) {
-                if (log.isTraceEnabled()) {
-                    log.trace("Matched path pattern [" + pathPattern + "] for requestURI [" + requestURI + "].  "
-                        + "Utilizing corresponding filter chain...");
+            if (pathPatternArray.length == NUM_2) {
+                if (WebUtils.toHttp(request).getMethod().toUpperCase().equals(pathPatternArray[1].toUpperCase())) {
+                    String pathPatternUrl = pathPatternArray[0];
+                    pathPatternUrl = safeFormatUrl(pathPatternUrl);
+                    if (pathMatches(pathPatternUrl, requestURI)) {
+                        if (log.isTraceEnabled()) {
+                            log.trace("Matched path pattern [" + pathPattern + "] for requestURI [" + requestURI + "]. "
+                                + "Utilizing corresponding filter chain...");
+                        }
+                        return filterChainManager.proxy(originalChain, pathPattern);
+                    }
                 }
-                return filterChainManager.proxy(originalChain, pathPattern);
             }
         }
         return null;
