c2f22431cd5fb219d33f6d3e3807106f8afb9f31
hankaibo <hankaibo@hotmail.com>
2020-06-08 14:52:28 +0800

    chore: 添加存储文件失败异常类型

--- src/main/java/cn/mypandora/springboot/config/exception/RestExceptionHandler.java
+++ src/main/java/cn/mypandora/springboot/config/exception/RestExceptionHandler.java
@@ -237,6 +237,13 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
         return buildResponseEntity(apiError);
     }
 
+    @ExceptionHandler(StorageException.class)
+    protected ResponseEntity<Object> handleStorageException(StorageException ex) {
+        ApiError apiError = new ApiError(HttpStatus.BAD_REQUEST);
+        apiError.setMessage(ex.getMessage());
+        return buildResponseEntity(apiError);
+    }
+
     private ResponseEntity<Object> buildResponseEntity(ApiError apiError) {
         return new ResponseEntity<>(apiError, apiError.getStatus());
     }
