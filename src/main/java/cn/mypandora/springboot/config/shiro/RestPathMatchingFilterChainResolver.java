6d80ed023bd2dbd20a121c72d3c4049f0f303ef5
hankaibo <hankaibo@hotmail.com>
2020-06-09 09:47:55 +0800

    fix: 解决shiro过滤器链匹配错误问题

--- src/main/java/cn/mypandora/springboot/config/shiro/RestPathMatchingFilterChainResolver.java
+++ src/main/java/cn/mypandora/springboot/config/shiro/RestPathMatchingFilterChainResolver.java
@@ -1,9 +1,12 @@
 package cn.mypandora.springboot.config.shiro;
 
+import java.util.Arrays;
+
 import javax.servlet.FilterChain;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.shiro.web.filter.mgt.FilterChainManager;
 import org.apache.shiro.web.filter.mgt.PathMatchingFilterChainResolver;
 import org.apache.shiro.web.util.WebUtils;
@@ -44,34 +47,51 @@ public class RestPathMatchingFilterChainResolver extends PathMatchingFilterChain
         if (!filterChainManager.hasChains()) {
             return null;
         }
-        // 扩写原匹配规则，在原来的路径一致的基础上加上请求方法也一致。
+
         String requestURI = getPathWithinApplication(request);
+
         requestURI = safeFormatUrl(requestURI);
 
         for (String pathPattern : filterChainManager.getChainNames()) {
             String[] pathPatternArray = pathPattern.split("==");
-            if (pathPatternArray.length == NUM_2) {
-                if (WebUtils.toHttp(request).getMethod().toUpperCase().equals(pathPatternArray[1].toUpperCase())) {
-                    String pathPatternUrl = pathPatternArray[0];
-                    pathPatternUrl = safeFormatUrl(pathPatternUrl);
-                    if (pathMatches(pathPatternUrl, requestURI)) {
-                        if (log.isTraceEnabled()) {
-                            log.trace("Matched path pattern [" + pathPattern + "] for requestURI [" + requestURI + "]. "
-                                + "Utilizing corresponding filter chain...");
-                        }
-                        return filterChainManager.proxy(originalChain, pathPattern);
+            // 不是/api/v1/xxx===GET形式的请求，则按原来shiro的匹配模式执行
+            if (pathPatternArray.length == 1) {
+                pathPattern = safeFormatUrl(pathPattern);
+                if (pathMatches(pathPattern, requestURI)) {
+                    if (log.isTraceEnabled()) {
+                        log.trace("Matched path pattern [" + pathPattern + "] for requestURI [" + requestURI + "]. "
+                            + "Utilizing corresponding filter chain...");
                     }
+                    // 只是路径一致
+                    return filterChainManager.proxy(originalChain, pathPattern);
+                }
+            } else {
+                // 自定义的请求接口形式，则除了匹配路径外还要匹配请求方法
+                int len = pathPatternArray.length;
+                String[] pathPatternUrlArray = Arrays.copyOf(pathPatternArray, len - 1);
+                String pathPatternUrl = StringUtils.join(pathPatternUrlArray, "==");
+                pathPatternUrl = safeFormatUrl(pathPatternUrl);
+                String pathPatternMethod = pathPatternArray[len - 1];
+                if (WebUtils.toHttp(request).getMethod().toUpperCase().equals(pathPatternMethod.toUpperCase())
+                    && pathMatches(pathPatternUrl, requestURI)) {
+                    if (log.isTraceEnabled()) {
+                        log.trace("Matched path pattern [" + pathPattern + "] for requestURI [" + requestURI + "]. "
+                            + "Utilizing corresponding filter chain...");
+                    }
+                    return filterChainManager.proxy(originalChain, pathPattern);
                 }
             }
         }
+        // 匹配不到进入servlet.
         return null;
     }
 
-    private String safeFormatUrl(String originUrl) {
-        if (originUrl != null && originUrl.endsWith(DEFAULT_PATH_SEPARATOR)) {
-            originUrl = originUrl.substring(0, originUrl.length() - 1);
+    private String safeFormatUrl(String requestURI) {
+        if (requestURI != null && !DEFAULT_PATH_SEPARATOR.equals(requestURI)
+            && requestURI.endsWith(DEFAULT_PATH_SEPARATOR)) {
+            requestURI = requestURI.substring(0, requestURI.length() - 1);
         }
-        return originUrl;
+        return requestURI;
     }
 
 }
